301b870dca36872dbbb4315cddc510f5c1df93d0
Ken V <git@kverb.com>
2024-03-01 16:08:02 -0500

    init Dockerfile and some configs

---
+++ config/fish/config.fish
@@ -0,0 +1,196 @@
+## Set values
+# Hide welcome message
+set fish_greeting
+set VIRTUAL_ENV_DISABLE_PROMPT "1"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANROFFOPT "-c"
+
+## Export variable need for qt-theme
+if type "qtile" >> /dev/null 2>&1
+   set -x QT_QPA_PLATFORMTHEME "qt5ct"
+end
+
+# Set settings for https://github.com/franciscolourenco/done
+set -U __done_min_cmd_duration 10000
+set -U __done_notification_urgency_level low
+
+
+## Environment setup
+# Apply .profile: use this to put fish compatible .profile stuff in
+if test -f ~/.fish_profile
+  source ~/.fish_profile
+end
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    fish_add_path ~/.local/bin
+end
+
+## Starship prompt
+if status --is-interactive
+   source ("/usr/bin/starship" init fish --print-full-init | psub)
+end
+
+
+## Functions
+# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Fish command history
+function history
+    builtin history --show-time='%F %T '
+end
+
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# Copy DIR1 DIR2
+function copy
+    set count (count $argv | tr -d \n)
+    if test "$count" = 2; and test -d "$argv[1]"
+	set from (echo $argv[1] | trim-right /)
+	set to (echo $argv[2])
+        command cp -r $from $to
+    else
+        command cp $argv
+    end
+end
+
+## Useful aliases
+# Replace ls with eza
+if command -sq eza
+    alias ls='eza -al --color=always --group-directories-first --icons -o --no-permissions --no-user' # preferred listing
+    alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+    alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+    alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+    alias l.="eza -a | egrep '^\.'"                                     # show only dotfiles
+end
+
+# Replace some more things with better alternatives
+if command -sq bat
+    set -x BAT_THEME "Coldark-Dark"
+    alias batt='bat --style header --style rule --style snip --style changes --style header'
+    alias cat='bat -pp'
+end
+[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
+
+# Common use
+alias grubup="sudo update-grub"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias tarnow='tar -acf '
+alias untar='tar -zxvf '
+alias wget='wget -c '
+alias rmpkg="sudo pacman -Rdd"
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias upd='/usr/bin/update'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                                   # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'			# List amount of -git packages
+
+# Get fastest mirrors 
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist" 
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist" 
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist" 
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist" 
+
+# Help people new to Arch
+alias apt='man pacman'
+alias apt-get='man pacman'
+alias please='sudo'
+alias tb='nc termbin.com 9999'
+
+# Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# Recent installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
+
+## Run paleofetch if session is interactive
+# if status --is-interactive
+#    neofetch
+# end
+
+fish_add_path (go env GOPATH)
+
+if command -sq gping
+    alias ping=gping
+end
+
+if command -sq zoxide
+    # https://github.com/ajeetdsouza/zoxide
+    zoxide init fish | source
+end
+
+# most distros already export this alias with packaged helix.
+# Unfortunately pacman does not 
+if command -sq helix
+    alias hx=helix
+end
+
+set -gx EDITOR vim
+
+alias svim="sudo -E vim"
+
+if command -sq ranger
+    alias r=ranger
+end
+
+if command -sq btm 
+    alias btmb="btm --battery --color gruvbox"
+end
+
+if command -sq fff
+    # Add this to you config.fish or equivalent.
+    # Fish don't support recursive calls so use f function
+    function f
+        fff $argv
+        set -q XDG_CACHE_HOME; or set XDG_CACHE_HOME $HOME/.cache
+        cd (cat $XDG_CACHE_HOME/fff/.fff_d)
+    end
+end
+
+if command -sq pack
+    source (pack completion --shell fish)
+end
+
